07d69b7d52be1ba0541059def49a254fe21dad80
Theodhor-90 <shyti.theodhor90@gmail.com>
2026-03-26 21:56:01 +0000

    Remove target/ and logs/ from tracking, add to .gitignore

--- target/debug/build/quote-791f82315cf5282a/build_script_build-791f82315cf5282a.d
+++
@@ -1,5 +0,0 @@
-/Users/theodhor/Desktop/Portfolio/Chess_Engine/target/debug/build/quote-791f82315cf5282a/build_script_build-791f82315cf5282a.d: /Users/theodhor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/Users/theodhor/Desktop/Portfolio/Chess_Engine/target/debug/build/quote-791f82315cf5282a/build_script_build-791f82315cf5282a: /Users/theodhor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/Users/theodhor/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
